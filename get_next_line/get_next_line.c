b59ef81acb4d8880d66629403525375e2da69b5b
Jiyeon Lee <jiyeolee@c1r5s5.42seoul.kr>
2023-01-24 18:22:43 +0900

    gnl malloc error

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jiyeolee <jiyeolee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 07:57:42 by jiyeolee          #+#    #+#             */
-/*   Updated: 2023/01/24 13:19:12 by jiyeolee         ###   ########.fr       */
+/*   Updated: 2023/01/24 18:21:28 by jiyeolee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,15 @@
 // 	*read_num = newline_idx + 1;
 // }
 
-int	check_buffer_find_newline(char *buf)
+static int	check_buffer_find_newline(char *buf, ssize_t read_num)
 {
 	ssize_t	i;
 
 	i = 0;
-	while (buf[i])
+	while (i < read_num)
 	{
-		if (buf[i] == '\n')
+		if (buf[i] == '\n' || buf[i] == '\0')
 		{
-			//buf[i + 1] = '\0';
 			return (i);
 		}
 		i++;
@@ -34,7 +33,7 @@ int	check_buffer_find_newline(char *buf)
 	return (-1);
 }
 
-ssize_t	save_line(int fd, t_backup *backup)
+static ssize_t	save_line(int fd, t_backup *backup)
 {
 	ssize_t	read_num;
 	ssize_t	newline_idx;
@@ -45,30 +44,38 @@ ssize_t	save_line(int fd, t_backup *backup)
 	if (read_num == -1)
 		return (-1);
 	if (read_num == 0)
-		return (0);
-	buf[read_num] = 0;
-	newline_idx = check_buffer_find_newline(buf);
-	if (newline_idx != -1)
 	{
-		read_num = newline_idx + 1;
+		return (0);
+	}
+	newline_idx = check_buffer_find_newline(buf, read_num);
+	if (newline_idx != -1) //개행 만나면 그만 읽고 리턴 시키기 
+	{
+		//read_num = newline_idx + 1;
 		buf[newline_idx + 1] = 0;
 		if (!backup->start)
 		{
-			backup->start = ft_strdup(buf);
+			save = ft_strdup(buf);
+			if (!save)
+				return (-1);
+			backup->start = save;
 			return (0);
 		}
-		// define_tail(newline_idx, &read_num, buf);
+		save = ft_strjoin(backup, buf, (size_t)newline_idx + 1);
+		if (!save)
+			return (-1);
+		if (backup->start)
+			free(backup->start);
+		backup->start = save;
+		return (0);
 	}
-	
-	//buffer에 뉴라인 잇나 확인
-	// 있으면 뉴라인 인덱스 리턴해서 거기까지 붙이기
-	//잇으면 return값 정하는 함수로 (개행까지 start에 넣고 종료) define_tail
-	
+	else
+		buf[read_num] = 0;
+
 	save = ft_strjoin(backup, buf, (size_t)read_num);
 	if (!save)
 		return (-1);
-	// if (!backup->start)
-	// 	free(backup->start);
+	if (backup->start)
+		free(backup->start);
 	backup->start = save;
 	// if (read_num < BUFFER_SIZE)
 	// 	return (0);
@@ -83,20 +90,23 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE < 0)
 		return (0);
-	
-	// 한줄씩 읽어오는 반복문	// read_num = read(fd, buf, BUFFER_SIZE);
-	// if (read_num == -1)
-	// 	return (0);
-	backup.start = 0;
-	backup.length = 0;
+	// backup.start = 0; 
+	// backup.length = 0;
 	while (1)
 	{
 		result = save_line(fd, &backup);
 		if (result == -1)
+		{
 			return (0);
+
+		}
 		if (result == 0)
 			break ;
 	}
 	return (backup.start);
 }
 
+// int	main(void)
+// {
+
+// }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jiyeolee <jiyeolee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 07:57:45 by jiyeolee          #+#    #+#             */
-/*   Updated: 2023/01/24 12:37:50 by jiyeolee         ###   ########.fr       */
+/*   Updated: 2023/01/24 17:51:50 by jiyeolee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-char	*get_next_line(int fd);
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000;
+# endif
 
 typedef struct s_backup
 {
@@ -27,14 +29,12 @@ typedef struct s_backup
 	//t_read	*next;
 }	t_backup;
 
+char	*get_next_line(int fd);
 char	*ft_strjoin(t_backup *backup, char *buf, size_t read_num);
 char	*ft_strdup(const char *s1);
 
 
 
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1000;
-# endif
 
 #endif
