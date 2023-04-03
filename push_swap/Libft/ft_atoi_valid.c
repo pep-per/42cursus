9dbe5c220fa3670d234d8d6ca987095ea2afe2c5
Jiyeon Lee <jiyeolee@c1r5s5.42seoul.kr>
2023-04-03 19:04:55 +0900

    error_fixing

--- push_swap/Libft/ft_atoi_valid.c
+++ push_swap/Libft/ft_atoi_valid.c
@@ -6,32 +6,26 @@
 /*   By: jiyeolee <jiyeolee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 16:50:48 by jiyeolee          #+#    #+#             */
-/*   Updated: 2023/04/01 23:08:59 by jiyeolee         ###   ########.fr       */
+/*   Updated: 2023/04/03 17:46:57 by jiyeolee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	check_sign(const char *str, int i)
+int	check_sign(const char *str, int *i)
 {
 	int	sign;
 
 	sign = 1;
-	if (str[i] == '-' || str[i] == '+')
+	if (str[*i] == '-' || str[*i] == '+')
 	{
-		if (str[i] == '-')
+		if (str[*i] == '-')
 			sign = -1;
-		if (!ft_isdigit(str[i + 1]))
+		(*i)++;
+		if (!ft_isdigit(str[*i]))
 			sign = 0;
 	}
 	return (sign);
-}
-
-int	is_empty_str(const char c)
-{
-	if (c == '\0')
-		return (1);
-	return (0);
 }
 
 int	ft_atoi_valid(const char *str, int *result)
@@ -44,17 +38,15 @@ int	ft_atoi_valid(const char *str, int *result)
 	i = 0;
 	while (ft_isspace(str[i]))
 		i++;
-	// a or "" : Error!
-	if (is_empty_str(str[i]))
-		return (0);
-	sign = check_sign(&str[i], i);
-	while (str[++i])
+	sign = check_sign(&str[i], &i);
+	while (str[i])
 	{
 		if (!ft_isdigit(str[i]))
 			break ;
 		num = (num * 10) + sign * (str[i] - '0');
 		if (num < INT_MIN || num > INT_MAX)
 			return (0);
+		i++;
 	}
 	while (ft_isspace(str[i]))
 		i++;
